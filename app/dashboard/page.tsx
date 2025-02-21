2dce7a8aaf211cb2bac1ec1a5500bfb6e218598a
Mohammed Adel <sublikira@gmail.com>
2025-02-21 15:30:18 +0200

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { poppins } from "@/app/ui/fonts";
+
+const Dashboard = () => {
+  return <div className={`${poppins.className}`}>Dashboard Page</div>;
+};
+
+export default Dashboard;
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter } from "next/font/google";
+
+import { Lusitana } from "next/font/google";
+
+import { Poppins } from "next/font/google";
+
+export const lusitana = Lusitana({ subsets: ["latin"], weight: "400" });
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const poppins = Poppins({ weight: "400" });
